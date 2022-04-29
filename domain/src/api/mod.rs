7caac6b200513570146abdc26f6a46e22177bcd9
William Henderson <william-henderson@outlook.com>
2022-04-29 19:51:53 +0100

    Refactored API command matching

--- domain/src/api/mod.rs
+++ domain/src/api/mod.rs
@@ -13,37 +13,53 @@ use humphrey_json::Value;
 
 use std::sync::Arc;
 
-pub fn http_api<F>(f: F) -> impl Fn(Request, Arc<State>) -> Response + Send + Sync + 'static
-where
-    F: Fn(Arc<State>, String) -> Value + Send + Sync + 'static,
-{
-    move |request: Request, state: Arc<State>| {
-        let string = request.content.and_then(|v| String::from_utf8(v).ok());
-
-        let response_body = if let Some(string) = string {
-            f(state, string)
-        } else {
-            json!({
-                "success": false,
-                "error": "No body"
-            })
-        };
-
-        let success = response_body
-            .get("success")
-            .and_then(|v| v.as_bool())
-            .unwrap_or(false);
-
-        let status_code = if success {
-            StatusCode::OK
-        } else {
-            StatusCode::BadRequest
-        };
-
-        Response::empty(status_code)
-            .with_bytes(response_body.serialize())
-            .with_header(HeaderType::ContentType, "application/json")
+pub fn matcher(command: &str) -> Box<dyn Fn(Arc<State>, String) -> Value + Send + Sync + 'static> {
+    match command {
+        "v1/signup" => Box::new(auth::signup),
+        "v1/login" => Box::new(auth::login),
+        "v1/logout" => Box::new(auth::logout),
+        "v1/user" => Box::new(user::get_user),
+        "v1/updateUser" => Box::new(user::update_user),
+        "v1/sets" => Box::new(sets::get_sets),
+        "v1/set" => Box::new(sets::get_set),
+        "v1/createSet" => Box::new(sets::create_set),
+        "v1/createSubset" => Box::new(sets::create_subset),
+        "v1/joinSet" => Box::new(sets::join_set),
+        "v1/leaveSet" => Box::new(sets::leave_set),
+        "v1/messages" => Box::new(messages::get_messages),
+        "v1/sendMessage" => Box::new(messages::send_message),
+        _ => Box::new(|_, _| not_found()),
     }
+}
+
+pub fn http_api(request: Request, state: Arc<State>) -> Response {
+    let route = request.uri.strip_prefix("/api/").unwrap();
+
+    let string = request.content.and_then(|v| String::from_utf8(v).ok());
+
+    let response_body = if let Some(string) = string {
+        (matcher(route))(state, string)
+    } else {
+        json!({
+            "success": false,
+            "error": "No body"
+        })
+    };
+
+    let success = response_body
+        .get("success")
+        .and_then(|v| v.as_bool())
+        .unwrap_or(false);
+
+    let status_code = if success {
+        StatusCode::OK
+    } else {
+        StatusCode::BadRequest
+    };
+
+    Response::empty(status_code)
+        .with_bytes(response_body.serialize())
+        .with_header(HeaderType::ContentType, "application/json")
 }
 
 pub fn error_context<F>(f: F) -> Value
@@ -57,6 +73,13 @@ where
             "error": e
         }),
     }
+}
+
+pub fn not_found() -> Value {
+    json!({
+        "success": false,
+        "error": "Invalid API command"
+    })
 }
 
 pub fn get_string(json: &Value, key: &str) -> Result<String, String> {
--- domain/src/main.rs
+++ domain/src/main.rs
@@ -24,20 +24,7 @@ pub struct State {
 fn main() -> Result<(), Box<dyn Error>> {
     let pool = Arc::new(Pool::new(Opts::from_url(DB_URL)?)?);
 
-    let app: App<State> = App::new_with_config(32, State { pool })
-        .with_route("/api/v1/signup", http_api(api::auth::signup))
-        .with_route("/api/v1/login", http_api(api::auth::login))
-        .with_route("/api/v1/logout", http_api(api::auth::logout))
-        .with_route("/api/v1/user", http_api(api::user::get_user))
-        .with_route("/api/v1/updateUser", http_api(api::user::update_user))
-        .with_route("/api/v1/sets", http_api(api::sets::get_sets))
-        .with_route("/api/v1/set", http_api(api::sets::get_set))
-        .with_route("/api/v1/createSet", http_api(api::sets::create_set))
-        .with_route("/api/v1/createSubset", http_api(api::sets::create_subset))
-        .with_route("/api/v1/joinSet", http_api(api::sets::join_set))
-        .with_route("/api/v1/leaveSet", http_api(api::sets::leave_set))
-        .with_route("/api/v1/messages", http_api(api::messages::get_messages))
-        .with_route("/api/v1/sendMessage", http_api(api::messages::send_message));
+    let app: App<State> = App::new_with_config(32, State { pool }).with_route("/api/*", http_api);
 
     app.run("0.0.0.0:80")
 }
