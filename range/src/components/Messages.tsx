3bbd08e421fc0b357320ebe7407169a9eddc71ee
William Henderson <william-henderson@outlook.com>
2022-05-14 18:00:16 +0100

    Fixed bug with not scrolling when changing subset

--- range/src/components/Messages.tsx
+++ range/src/components/Messages.tsx
@@ -96,6 +96,15 @@ class Messages extends React.Component<MessagesProps, MessagesState> {
     if (this.state.scrollLockedToBottom) {
       this.messages.current!.scrollTop = this.messages.current!.scrollHeight;
     }
+
+    if (this.props.subset !== undefined) {
+      if (this.lastId !== this.props.subset.id) {
+        this.lastId = this.props.subset.id;
+        this.setState({ scrollLockedToBottom: true });
+      } else {
+        this.lastId = this.props.subset.id;
+      }
+    }
   }
 
   render() {
@@ -131,7 +140,7 @@ class Messages extends React.Component<MessagesProps, MessagesState> {
 
           {!this.state.scrollLockedToBottom &&
             <div className="scrollDownButton" onClick={this.lockScroll}>
-              <svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12.25 5.5V18M12.25 18L6.25 12M12.25 18L18.25 12" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </div>
