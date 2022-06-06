0189b4b9e61ab4e384e88d2dffd1c54e6ee8ed15
William Henderson <william-henderson@outlook.com>
2022-06-06 21:37:45 +0100

    Designed first draft of download page

--- axiom/src/pages/index.tsx
+++ axiom/src/pages/index.tsx
@@ -1,195 +1,133 @@
-import * as React from "react"
+import React from "react";
+import Helmet from "react-helmet";
+import "../styles/index.scss";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import icon from "../images/icon.png";
+import screenshot from "../images/screenshot.png";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+import { DownloadIcon, LaTeXIcon, VoiceIcon, SpeedIcon } from "./svg";
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+class Index extends React.Component {
+  render() {
+    return (
+      <main>
+        <Helmet>
+          <title>Equion | Integrate Together</title>
+          <meta name="description" content="Equion, an intuitive chat platform for work and play. With built-in support for LaTeX, high-quality voice and video chat with up to 50% lower latency than Discord, and a stylish and snappy interface, Equion is the perfect platform for any group." />
+          <link rel="icon" href={icon} />
+        </Helmet>
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+        <nav>
+          <div>
+            <img src={icon} alt="Equion" />
+            <h2>Equion</h2>
+          </div>
 
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
+          <ul>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Download</a></li>
+          </ul>
+        </nav>
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+        <section className="hero">
+          <div className="title">
+            <img src={icon} alt="Equion" />
+            <h1>Equion</h1>
+          </div>
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+          <span>Integrate Together.</span>
 
-// markup
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        🎉🎉🎉
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
+          <div className="bigButtons">
+            <a href="#">
+              <DownloadIcon />
+              <span>Download for Windows</span>
             </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+
+            <a href="#" className="disabled">
+              <DownloadIcon />
+              <span>Other Platforms Coming Soon</span>
+            </a>
+          </div>
+
+          <img src={screenshot} alt="Equion screenshot" />
+        </section>
+
+        <section>
+          <div className="title">
+            <h2>Features</h2>
+            <span>An intuitive chat platform for work and play.</span>
+          </div>
+
+          <span>
+            With built-in support for LaTeX, high-quality voice and video chat with up to 50% lower latency than Discord<sup>1</sup>, and a stylish and snappy interface, Equion is the perfect platform for any group.
+          </span>
+
+          <div className="features">
+            <div>
+              <div>
+                <LaTeXIcon />
+              </div>
+
+              <h3>Native support for LaTeX and Markdown.</h3>
+              <span>The days of incomprehensible equations in ASCII are over. Equion makes it easy to share complex ideas in an easy-to-read format.</span>
+            </div>
+
+            <div>
+              <div>
+                <VoiceIcon />
+              </div>
+
+              <h3>High-quality, low-latency voice chat and screen sharing.</h3>
+              <span>Peer-to-peer technology allows for lower latency and better privacy for those problems you can't solve over text.</span>
+            </div>
+
+            <div>
+              <div>
+                <SpeedIcon />
+              </div>
+
+              <h3>Lightweight and lightning-fast.</h3>
+              <span>With the entire app fitting within Discord's 8 MB attachment size limit, and using just 3% of the RAM that Discord uses<sup>2</sup>, Equion won't slow you down.</span>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="title">
+            <h2>Download</h2>
+            <span>Start integrating today for free.</span>
+          </div>
+
+          <div className="bigButtons">
+            <a href="#">
+              <DownloadIcon />
+              <span>Download for Windows</span>
+            </a>
+          </div>
+        </section>
+
+        <footer>
+          <div className="citations">
+            <p>
+              <sup>1</sup>: Based on two clients geographically close together to maximise the benefits of peer-to-peer networking.
+            </p>
+
+            <p>
+              <sup>2</sup>: Calculated on the latest versions of Equion and Discord idle and minimised, Equion used 4 MB maximum RAM while Discord used 140 MB maximum RAM.
+            </p>
+          </div>
+
+          <hr />
+
+          <div className="bottom">
+            <img src={icon} alt="Equion" />
+            <h2>Equion</h2>
+            <span>&copy; William Henderson 2022</span>
+          </div>
+        </footer>
+      </main>
+    )
+  }
 }
 
-export default IndexPage
+export default Index;
---
+++ axiom/src/pages/svg.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export function DownloadIcon() {
+  return (
+    <svg width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 20L18 20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 4V16M12 16L15.5 12.5M12 16L8.5 12.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
+
+export function LaTeXIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 4L12 4L20 4V7" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M4 20H12H20V17" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M4 20L12 12L4 4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
+
+export function VoiceIcon() {
+  return (
+    <svg width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 4L12 20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M8 9L8 15" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M20 10L20 14" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M4 10L4 14" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M16 7L16 17" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
+
+export function SpeedIcon() {
+  return (
+    <svg width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 7.01L12.01 6.99889" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M16 9.01L16.01 8.99889" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M8 9.01L8.01 8.99889" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M18 13.01L18.01 12.9989" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M6 13.01L6.01 12.9989" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M17 17.01L17.01 16.9989" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M7 17.01L7.01 16.9989" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 17L13 11" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M8.5 20.001H4C2.74418 18.3295 2 16.2516 2 14C2 8.47715 6.47715 4 12 4C17.5228 4 22 8.47715 22 14C22 16.2516 21.2558 18.3295 20 20.001L15.5 20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 23C13.6569 23 15 21.6569 15 20C15 18.3431 13.6569 17 12 17C10.3431 17 9 18.3431 9 20C9 21.6569 10.3431 23 12 23Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
