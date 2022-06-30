54f4d3a7fe475c920fbddb903bfca3bfbf58f43c
William Henderson <william-henderson@outlook.com>
2022-06-30 17:46:35 +0100

    Made test harness work with nested arrays

--- domain/src/tests/harness.rs
+++ domain/src/tests/harness.rs
@@ -43,33 +43,38 @@ pub(crate) fn harness(stages: impl Iterator<Item = TestStage>) {
                 output: stage_output,
             } => {
                 // Add variables to input if necessary
-                let input = if !variables.is_empty() {
+                let (stage_input, stage_output) = if !variables.is_empty() {
                     let mut input_string = stage_input.serialize();
+                    let mut output_string = stage_output.serialize();
 
                     for (key, value) in variables.iter() {
                         input_string = input_string.replace(&format!("{{{{{}}}}}", key), value);
+                        output_string = output_string.replace(&format!("{{{{{}}}}}", key), value);
                     }
 
-                    humphrey_json::from_str(input_string).unwrap()
+                    (
+                        humphrey_json::from_str(input_string).unwrap(),
+                        humphrey_json::from_str(output_string).unwrap(),
+                    )
                 } else {
-                    stage_input.clone()
+                    (stage_input, stage_output)
                 };
 
                 // Run the handler to get the output
                 let output = crate::ws::handler_internal(
-                    Some(input.clone()),
+                    Some(stage_input.clone()),
                     addr.unwrap_or_else(|| {
                         "127.0.0.1:12345".to_socket_addrs().unwrap().next().unwrap()
                     }),
                     state.clone(),
                 );
 
-                println!("Input: {}", input.serialize());
+                println!("Input: {}", stage_input.serialize());
                 println!("Output: {}", output.serialize());
                 println!("Expected Output: {}\n", stage_output.serialize());
 
                 // Update variables if necessary and check for equality
-                for (k, v) in equality_and_variables(&output, &stage_output) {
+                for (k, v) in update_variables(&output, &stage_output).unwrap() {
                     variables.insert(k, v);
                 }
 
@@ -92,7 +97,7 @@ pub(crate) fn harness(stages: impl Iterator<Item = TestStage>) {
                     println!("Expected Event: {}\n", stage_data.serialize());
 
                     // Update variables if necessary and check for equality
-                    for (k, v) in equality_and_variables(&data, &stage_data) {
+                    for (k, v) in update_variables(&data, &stage_data).unwrap() {
                         variables.insert(k, v);
                     }
                 } else {
@@ -103,30 +108,32 @@ pub(crate) fn harness(stages: impl Iterator<Item = TestStage>) {
     }
 }
 
-fn equality_and_variables(v1: &Value, v2: &Value) -> Vec<(String, String)> {
-    let mut variables = Vec::new();
+fn update_variables(v1: &Value, v2: &Value) -> Result<Vec<(String, String)>, String> {
+    let mut variables: Vec<(String, String)> = Vec::new();
 
     if let Value::Object(v1_object) = v1 {
         if let Value::Object(v2_object) = v2 {
             let v1_object = v1_object.clone();
             let mut v2_object = v2_object.clone();
 
-            assert_eq!(v1_object.len(), v2_object.len());
+            if v1_object.len() != v2_object.len() {
+                return Err("Objects have different lengths".to_string());
+            }
 
             for (k, v) in v1_object {
                 if let Some(index) = v2_object.iter().position(|(k2, _)| *k2 == k) {
                     let (_, v2) = v2_object.swap_remove(index);
 
-                    variables.extend(equality_and_variables(&v, &v2));
+                    variables.extend(update_variables(&v, &v2)?);
                 } else {
-                    panic!("Resultant JSON missing key {}", k)
+                    return Err(format!("Resultant JSON missing key {}", k));
                 }
             }
 
-            return variables;
+            return Ok(variables);
         }
 
-        panic!("Resultant JSON is not an object")
+        return Err("Resultant JSON is not an object".to_string());
     }
 
     if let Value::Array(v1_a) = v1 {
@@ -134,26 +141,28 @@ fn equality_and_variables(v1: &Value, v2: &Value) -> Vec<(String, String)> {
             let v1_a = v1_a.clone();
             let mut v2_a = v2_a.clone();
 
-            assert_eq!(v1_a.len(), v2_a.len());
+            if v1_a.len() != v2_a.len() {
+                return Err("Arrays have different lengths".to_string());
+            }
 
             for v in v1_a {
-                if let Some(index) = v2_a.iter().position(|v2| v == *v2) {
-                    v2_a.swap_remove(index);
+                if let Some(index) = v2_a.iter().position(|v2| update_variables(&v, v2).is_ok()) {
+                    variables.extend(update_variables(&v, &v2_a.swap_remove(index))?);
                 } else {
-                    panic!("Resultant JSON missing value {}", v.serialize());
+                    return Err(format!("Resultant JSON missing value {}", v.serialize()));
                 }
             }
 
-            return variables;
+            return Ok(variables);
         }
 
-        panic!("Resultant JSON is not an array")
+        return Err("Resultant JSON is not an array".to_string());
     }
 
     if let Value::String(v1_s) = v1 {
         if let Value::String(v2_s) = v2 {
             if v2_s == "*" {
-                return variables;
+                return Ok(variables);
             }
 
             if v2_s.starts_with("{{") && v2_s.ends_with("}}") {
@@ -166,18 +175,29 @@ fn equality_and_variables(v1: &Value, v2: &Value) -> Vec<(String, String)> {
                     v1_s.to_string(),
                 ));
 
-                return variables;
+                return Ok(variables);
             }
 
-            assert_eq!(v1_s, v2_s);
+            if v1_s != v2_s {
+                return Err(format!(
+                    "Resultant JSON has different value, expected {} but got {}",
+                    v2_s, v1_s
+                ));
+            }
 
-            return variables;
+            return Ok(variables);
         }
 
-        panic!("Resultant JSON is not a string")
+        return Err("Resultant JSON is not a string".to_string());
     }
 
-    assert_eq!(v1, v2);
+    if v1 != v2 {
+        return Err(format!(
+            "Resultant JSON has different value, expected {} but got {}",
+            v2.serialize(),
+            v1.serialize()
+        ));
+    }
 
-    variables
+    Ok(variables)
 }
